a886cdf12b2239fc0fb10cdef6e89eaed498c7cb
koral <koral@mailoo.org>
2011-03-30 19:52:49 +0200

    Added uzbl configuration.

---
+++ uzbl/scripts/my_download.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Options
+useragent="Firefox"
+target="$HOME"
+
+# Try to add URI to running instance of aria
+notify-send --icon /usr/share/uzbl/examples/data/uzbl.png "Downloading ($3)" "$2\n$4 bytes"
+aria2rpc addUri $1
+
+# If it failed, aria probably isn't running yet
+if [ $? -gt 0 ]; then
+    # Start new instance of aria
+    aria2c -D -c \
+        -d $target \
+        -U $useragent \
+        --enable-xml-rpc \
+        >/dev/null 2>&1
+
+    # Now try again
+    aria2rpc addUri $1
+
+    # If it failed once more, fall back to wget
+    if [ $? -gt 0 ]; then
+        ( cd $target && wget --user-agent $useragent $1 )
+    fi
+fi
