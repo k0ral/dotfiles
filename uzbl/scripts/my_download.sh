f1c1f3b84aaa04a736811ef8dab1530d45849d1c
koral <koral att mailoo dott org>
2011-07-18 17:50:17 +0200

    [uzbl] Back to basic download handler using wget.

--- uzbl/scripts/my_download.sh
+++ uzbl/scripts/my_download.sh
@@ -6,23 +6,25 @@ target=$HOME
 
 # Try to add URI to running instance of aria
 notify-send --icon /usr/share/uzbl/examples/data/uzbl.png "Downloading ($3)" "$2\n$4 bytes"
-aria2rpc addUri "$1" -o "$2"
+#aria2rpc addUri "$1" -o "$2"
+cd $target
+wget --user-agent $useragent $1
 
 # If it failed, aria probably isn't running yet
-if [ $? -gt 0 ]; then
-    # Start new instance of aria
-    aria2c -D -c \
-        -d $target \
-        -U $useragent \
-        --enable-rpc \
+#if [ $? -gt 0 ]; then
+#    # Start new instance of aria
+#    aria2c -D -c \
+#        -d $target \
+#        -U $useragent \
+#        --enable-rpc \
 
-        #>/dev/null 2>&1
+#        #>/dev/null 2>&1
 
-    # Now try again
-    aria2rpc addUri "$1" -o "$2"
+#    # Now try again
+#    aria2rpc addUri "$1" -o "$2"
 
-    # If it failed once more, fall back to wget
-    if [ $? -gt 0 ]; then
-        ( cd $target && wget --user-agent $useragent $1 )
-    fi
-fi
+#    # If it failed once more, fall back to wget
+#    if [ $? -gt 0 ]; then
+#        ( cd $target && wget --user-agent $useragent $1 )
+#    fi
+#fi
