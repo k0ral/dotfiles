4d57d8fa6777b257641129ff2d7a109f11c7261d
koral <koral att mailoo dott org>
2012-08-25 01:04:17 +0200

    [acpi] Updated handler.

--- etc/acpi/handler.sh
+++ etc/acpi/handler.sh
@@ -9,17 +9,24 @@ set $*
 
 case "$1" in
     button/power)
-        #echo "PowerButton pressed!">/dev/tty5
         case "$2" in
-            PBTN|PWRF)  logger "PowerButton pressed: $2" ;;
-            *)          logger "ACPI action undefined: $2" ;;
+            PBTN|PWRF)
+                logger "PowerButton pressed: $2"
+                #poweroff
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
         esac
         ;;
     button/sleep)
         case "$2" in
-            #SLPB)   echo -n mem >/sys/power/state ;;
-            SLPB)   pm-hibernate ;;
-            *)      logger "ACPI action undefined: $2" ;;
+            SLPB|SBTN)
+                echo -n mem >/sys/power/state
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
         esac
         ;;
     ac_adapter)
@@ -44,26 +51,37 @@ case "$1" in
                     ;;
                 esac
                 ;;
-            *)  logger "ACPI action undefined: $2" ;;
+            *)
+		logger "ACPI action undefined: $2" 
+		;;
         esac
         ;;
     battery)
         case "$2" in
             BAT0)
                 case "$4" in
-                    00000000)   #echo "offline" >/dev/tty5
-                    ;;
-                    00000001)   #echo "online"  >/dev/tty5
-                    ;;
+                    00000000)
+                        logger 'Battery online'
+                        ;;
+                    00000001)
+                        logger 'Battery offline'
+                        ;;
                 esac
                 ;;
-            CPU0)	
+            CPU0)
                 ;;
             *)  logger "ACPI action undefined: $2" ;;
         esac
         ;;
     button/lid)
-        #echo "LID switched!">/dev/tty5
+        case "$3" in
+            close)
+                logger 'LID closed'
+                ;;
+            open)
+                logger 'LID opened'
+                ;;
+        esac
         ;;
     hotkey)
         case "$3" in
