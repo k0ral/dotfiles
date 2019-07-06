663c06393b7c314ce9648b7f37d5a4ba88526017
koral <chahine.moreau@gmail.com>
2019-07-06 11:08:24 +0200

    [qutebrowser] Disable javascript by default.

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -609,7 +609,7 @@ c.content.autoplay = False
 
 ## Enable JavaScript.
 ## Type: Bool
-# c.content.javascript.enabled = True
+c.content.javascript.enabled = False
 
 ## Log levels to use for JavaScript console logging messages. When a
 ## JavaScript message with the level given in the dictionary key is
