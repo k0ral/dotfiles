90c9fc961cd8d39d8b0057df4d2391ec743fa754
koral <mail@cmoreau.info>
2020-05-30 17:46:54 +0200

    [nixos] Remove old configuration for android development

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -259,10 +259,4 @@
     isNormalUser = true;
     uid = 1000;
   };
-
-  # Android development
-  # environment.systemPackages = with pkgs; [ androidenv.platformTools heimdall ];
-  # services.udev.packages = [ pkgs.android-udev-rules ];
-  # users.extraGroups.plugdev = { };
-  # users.extraUsers.koral.extraGroups = [ "plugdev" ];
 }
